04ba5477879bcad4984b687a98d64d0244aff670
sweesweett <sweesweet@naver.com>
2022-06-20 00:49:20 +0900

    일기 비었을 때 설정

--- src/Pages/DiaryList.js
+++ src/Pages/DiaryList.js
@@ -4,17 +4,24 @@ import Diary from '../Components/Diary';
 const DiaryList = ({ state, setState }) => {
   return (
     <ul className='DiaryList'>
-      {state.map((el, index) => {
-        return (
-          <Diary
-            key={el.origin_id}
-            el={el}
-            index={index}
-            state={state}
-            setState={setState}
-          />
-        );
-      })}
+      {state.length !== 0 ? (
+        state.map((el, index) => {
+          return (
+            <Diary
+              key={el.origin_id}
+              el={el}
+              index={index}
+              state={state}
+              setState={setState}
+            />
+          );
+        })
+      ) : (
+        <div className='noDiary'>
+          쌓여있는 일기가 없어요!
+          <br /> ಥ_ಥ
+        </div>
+      )}
     </ul>
   );
 };
